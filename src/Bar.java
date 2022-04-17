4c640afad01ba8db653d75a0ae2d7de5417d3e8b
addthoriq <alvarro.thoriq@gmail.com>
2022-04-17 16:29:52 +0700

    Delete or Reset done

--- src/Bar.java
+++ src/Bar.java
@@ -3,58 +3,62 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Bar extends Canvas{
-    public Bar(String jan, String feb, String mar, String apr, String mei, String jun){
-        int ja = Integer.parseInt(jan);
-        int fe = Integer.parseInt(feb);
-        int mr = Integer.parseInt(mar);
-        int ap = Integer.parseInt(apr);
-        int mi = Integer.parseInt(mei);
-        int jn = Integer.parseInt(jun);
-        getMax(ja, fe, mr, ap, mi, jn);
+    int[] arrJum = new int[6];
+    int minValue = 0, maxValue = 0;
+
+    public Bar(int[] arrValue){
+        arrJum = arrValue;
+        // for (int index = 0; index < arrValue.length; index++)
+        //     this.arrJum[index] = Integer.parseInt(arrValue[index]);
+        getMaxMin();
         setBounds(222, 30, 275, 272);
         setBackground(Color.white);
         setVisible(true);
         setFocusable(false);
     }
 
-    int max;
+    // public void receivedData(String[] arrValue){
+        // for (int index = 0; index < arrValue.length; index++)
+        //     this.arrJum[index] = Integer.parseInt(arrValue[index]);
+        // getMaxMin();
+    // }
 
-    public void getMax(int jan, int feb, int mar, int apr, int mei, int jun){
-        int[] arr = {jan, feb, mar, apr, mei, jun};
-        int m = arr[0];
-        for (int i = 0; i < 6; i++) {
-            if (arr[i] > m){
-                m = arr[i];
-            }
+    public void getMaxMin(){
+        for (int i = 0; i < this.arrJum.length; i++) {
+            if (this.minValue > this.arrJum[i])
+                this.minValue = this.arrJum[i];
+            if (this.maxValue < this.arrJum[i])
+                this.maxValue = this.arrJum[i];
         }
-        max = m;
     }
 
-    public String getMid(){
-        return Integer.toString(max / 2);
+    public int getMid(){
+        return this.maxValue / 2;
     }
 
     @Override
     public void paint (Graphics g){
+        int tepiAtas = 45;
+        int tepiBawah = 240;
         // Start Judul Atas
-        g.drawString("Grafik Penjualan 6 Bulan", 60, 30);
+        g.drawString("Grafik Penjualan 6 Bulan", 60, 25);
         // End Judul Atas
         /* Start Nilai Samping */
         // Nilai Samping Max
-        g.drawString(Integer.toString(max), (25 / 2) - 3, 45);
+        g.drawString(Integer.toString(this.maxValue), (25 / 2) - 3, tepiAtas);
         // Nilai Samping Mid
-        int nilaiMid = (237 - 45) / 2 + 45;
-        g.drawString(getMid(), (25 / 2) - 3, nilaiMid);
+        int nilaiMid = (tepiBawah - tepiAtas) / 2 + (this.maxValue - this.minValue);
+        g.drawString(Integer.toString(getMid()), (25 / 2) - 3, nilaiMid);
         /* End Nilai Samping */
 
         // Start Warna Garis
-        g.setColor(Color.red);
+        g.setColor(Color.blue);
         // End Warna Garis
         // Start Garis Y
-        g.drawLine(25, 237, 25, 45);
+        g.drawLine(25, tepiBawah - 7, 25, tepiAtas);
         // End Garis 
         // Start Garis X
-        g.drawLine(25, 237, 247, 237);
+        g.drawLine(25, tepiBawah - 7, 247, tepiBawah - 7);
         // End Garis X
         
         /*
@@ -65,13 +69,29 @@ public class Bar extends Canvas{
         */
 
         // Start Bar Grafik
-        g.setColor(Color.blue);
-        g.fillRect(27, 45, 35, (237 - 45) - 1); // Bar Full
-        g.fillRect(64, 45 + 96, 35, 237 - 141 - 1); // 237 Garis Bawah - koor Y - spasi
-        g.fillRect(101, 45 + 120, 35, 237 - (45 + 120) - 1);
-        g.fillRect(138, 45 + 140, 35, 237 - (45 + 140) - 1);
-        g.fillRect(175, 45 + 169, 35, 237 - (45 + 169) - 1);
-        g.fillRect(212, 45 + 180, 35, 237 - (45 + 180) - 1);
+        g.setColor(Color.red);
+
+        int ukuran = (tepiBawah - tepiAtas) / (this.maxValue - this.minValue);
+        int lebarBatang = 37;
+        String[] namaBulan = {"Jan", "Feb", "Mar", "Apr", "Mei", "Jun"};
+        
+        for (int index = 0; index < this.arrJum.length; index++){
+            int koorX = index * lebarBatang + 27;
+            int koorY = tepiAtas;
+            int tinggi = (int) (this.arrJum[index] * ukuran);
+            if (this.arrJum[index] >= 0) {
+                koorY += (int) ((this.maxValue - this.arrJum[index]) * ukuran);
+            } else {
+                koorY += (int) (this.maxValue * ukuran);
+                tinggi = -tinggi;
+            }
+            g.fillRect(koorX, koorY, lebarBatang - 1, tinggi);
+            g.drawString(namaBulan[index], koorX + 7, 250);
+        }
         // End Bar Grafik
     }
+
+    // public void run(int[] arrValue){
+    //     arrJum = arrValue;
+    // }
 }
--- src/Grafik.java
+++ src/Grafik.java
@@ -1,17 +1,41 @@
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
+import java.awt.*;
 
-public class Grafik {
-    public static void main ( String [] args ) {
-        JFrame frame = new JFrame();
-        frame.setTitle("Thoriq, Fajri, Yunika");
-        frame.setSize(650, 365);
-        frame.setLayout(null);
+public class Grafik extends JFrame implements ActionListener {
+
+    JButton btn_bar = new JButton();
+    JButton btn_line = new JButton();
+    JButton btn_pie = new JButton();
+    JButton btn_hapus = new JButton();
+    JTextField tx_Jan = new JTextField();
+    JTextField tx_Feb = new JTextField();
+    JTextField tx_Mar = new JTextField();
+    JTextField tx_Apr = new JTextField();
+    JTextField tx_Mei = new JTextField();
+    JTextField tx_Jun = new JTextField();
+    Canvas c = new Canvas();
+
+    int[] arrValue;
+
+    public void setValue(int ja, int fe, int mr, int ap, int me, int jn){
+        arrValue = new int[] {ja, fe, mr, ap, me, jn};
+    }
+
+    Grafik(){
+        setTitle("Thoriq, Fajri, Yunika");
+        setSize(650, 365);
+        setLayout(null);
+
+        c.setBounds(222, 30, 275, 272);
+        c.setBackground(Color.white);
+        c.setVisible(true);
+        c.setFocusable(false);
         
-        String[] namaBulan = {"Januari", "Februari", "Maret", "April", "Mei", "Juni"};
-        
+
+        String[] namaBulan = { "Januari", "Februari", "Maret", "April", "Mei", "Juni" };
+
         /* Start Judul Diatas */
         // Start Penjualan
         JLabel titlePenjualan = new JLabel();
@@ -26,13 +50,12 @@ public class Grafik {
         // End Tombol
         /* End Judul Diatas */
 
-
         /* Data Penjualan 6 Bulan */
         // Start Januari
         JLabel label_Jan = new JLabel();
         label_Jan.setBounds(20, 70, 80, 15);
         label_Jan.setText(namaBulan[0]);
-        JTextField tx_Jan = new JTextField();
+        
         tx_Jan.setText("3");
         tx_Jan.setBounds(120, 62, 60, 30);
         // End Januari
@@ -41,7 +64,7 @@ public class Grafik {
         JLabel label_Feb = new JLabel();
         label_Feb.setBounds(20, 111, 80, 15);
         label_Feb.setText(namaBulan[1]);
-        JTextField tx_Feb = new JTextField();
+        
         tx_Feb.setText("2");
         tx_Feb.setBounds(120, 103, 60, 30);
         // End Februari
@@ -50,7 +73,7 @@ public class Grafik {
         JLabel label_Mar = new JLabel();
         label_Mar.setBounds(20, 151, 80, 15);
         label_Mar.setText(namaBulan[2]);
-        JTextField tx_Mar = new JTextField();
+        
         tx_Mar.setText("7");
         tx_Mar.setBounds(120, 144, 60, 30);
         // End Maret
@@ -59,7 +82,7 @@ public class Grafik {
         JLabel label_Apr = new JLabel();
         label_Apr.setBounds(20, 192, 80, 15);
         label_Apr.setText(namaBulan[3]);
-        JTextField tx_Apr = new JTextField();
+        
         tx_Apr.setText("5");
         tx_Apr.setBounds(120, 185, 60, 30);
         // End April
@@ -68,7 +91,7 @@ public class Grafik {
         JLabel label_Mei = new JLabel();
         label_Mei.setBounds(20, 233, 80, 15);
         label_Mei.setText(namaBulan[4]);
-        JTextField tx_Mei = new JTextField();
+        
         tx_Mei.setText("11");
         tx_Mei.setBounds(120, 226, 60, 30);
         // End Mei
@@ -77,91 +100,180 @@ public class Grafik {
         JLabel label_Jun = new JLabel();
         label_Jun.setBounds(20, 274, 80, 15);
         label_Jun.setText(namaBulan[5]);
-        JTextField tx_Jun = new JTextField();
+        
         tx_Jun.setText("4");
         tx_Jun.setBounds(120, 267, 60, 30);
         // End Juni
         /* End Data Penjualan 6 Bulan */
 
+        
+
         /* Start Tombol-Tombol */
         // Start Tombol Bar
-        JButton btn_bar = new JButton();
+        
         btn_bar.setBounds(542, 62, 80, 30);
         btn_bar.setText("Bar");
-        btn_bar.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
+        btn_bar.addActionListener(this);
         // End Tombol Bar
         // Start Tombol Line
-        JButton btn_line = new JButton();
         btn_line.setBounds(542, 103, 80, 30);
         btn_line.setText("Line");
-        btn_line.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                
-            }
-        });
+        btn_line.addActionListener(this);
         // End Tombol Line
         // Start Pie
-        JButton btn_pie = new JButton();
+        
         btn_pie.setBounds(542, 144, 80, 30);
         btn_pie.setText("Pie");
-        btn_pie.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
+        btn_pie.addActionListener(this);
         // End Pie
         // Start Hapus
-        JButton btn_hapus = new JButton();
+        
         btn_hapus.setBounds(542, 267, 80, 30);
         btn_hapus.setText("Hapus");
-        btn_hapus.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
+        btn_hapus.addActionListener(this);
         // End Hapus
         /* End Tombol */
-        
-        // Start Canvas
-        Bar kanvas_Bar = new Bar(tx_Jan.getText(), tx_Feb.getText(), tx_Mar.getText(), tx_Apr.getText(), tx_Mei.getText(), tx_Jun.getText());
-        // End Canvas
-        
-        // Start Frame Config
-        /* Start Data Penjualan Add Frame */
-        frame.add(tx_Jan);
-        frame.add(label_Jan);
-        frame.add(tx_Feb);
-        frame.add(label_Feb);
-        frame.add(tx_Mar);
-        frame.add(label_Mar);
-        frame.add(tx_Apr);
-        frame.add(label_Apr);
-        frame.add(tx_Mei);
-        frame.add(label_Mei);
-        frame.add(tx_Jun);
-        frame.add(label_Jun);
-        /* End Data Penjualan Add Frame */
-        
-        /* Start Tombol Frame */
-        frame.add(btn_bar);
-        frame.add(btn_line);
-        frame.add(btn_pie);
-        frame.add(btn_hapus);
-        /* End Tombol Frame */
 
-        frame.add(titlePenjualan);
-        frame.add(titleMode);
-        frame.add(kanvas_Bar);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        // Start Canvas
+        int ja = Integer.parseInt(tx_Jan.getText());
+        int fe = Integer.parseInt(tx_Feb.getText());
+        int mr = Integer.parseInt(tx_Mar.getText());
+        int ap = Integer.parseInt(tx_Apr.getText());
+        int me = Integer.parseInt(tx_Mei.getText());
+        int jn = Integer.parseInt(tx_Jun.getText());
+        setValue(ja, fe, mr, ap, me, jn);
+        // End Canvas
+
+        // Start Config
+        /* Start Data Penjualan Add */
+        add(tx_Jan);
+        add(label_Jan);
+        add(tx_Feb);
+        add(label_Feb);
+        add(tx_Mar);
+        add(label_Mar);
+        add(tx_Apr);
+        add(label_Apr);
+        add(tx_Mei);
+        add(label_Mei);
+        add(tx_Jun);
+        add(label_Jun);
+        /* End Data Penjualan Add */
+
+        /* Start Tombol */
+        add(btn_bar);
+        add(btn_line);
+        add(btn_pie);
+        add(btn_hapus);
+        /* End Tombol */
+
+        add(c);
+
+        add(titlePenjualan);
+        add(titleMode);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
         // End Frame Config
+    }
+
+    int minValue = 0, maxValue = 0;
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        Graphics g = c.getGraphics();
+        if (source == btn_bar){
+            int ja = Integer.parseInt(tx_Jan.getText());
+            int fe = Integer.parseInt(tx_Feb.getText());
+            int mr = Integer.parseInt(tx_Mar.getText());
+            int ap = Integer.parseInt(tx_Apr.getText());
+            int me = Integer.parseInt(tx_Mei.getText());
+            int jn = Integer.parseInt(tx_Jun.getText());
+            setValue(ja, fe, mr, ap, me, jn);
+            getBar(g);
+        } else if (source == btn_line){
+            System.exit(0);
+        } else if (source == btn_pie){
+            System.exit(0);
+        } else {
+            g.setColor(Color.WHITE);
+            g.fillRect(0, 0, c.getWidth(), c.getHeight());
+            this.maxValue = 0;
+            this.minValue = 0;
+        }
+    }
+
+    public void getMaxMin() {
+        for (int i = 0; i < this.arrValue.length; i++) {
+            if (this.minValue > this.arrValue[i])
+                this.minValue = this.arrValue[i];
+            if (this.maxValue < this.arrValue[i])
+                this.maxValue = this.arrValue[i];
+        }
+    }
+
+    public int getMid() {
+        return this.maxValue / 2;
+    }
+
+    public void getBar(Graphics g){
+        getMaxMin();
+        getMid();
+        int tepiAtas = 45;
+        int tepiBawah = 240;
+        // Start Judul Atas
+        g.drawString("Grafik Penjualan 6 Bulan", 60, 25);
+        // End Judul Atas
+        /* Start Nilai Samping */
+        // Nilai Samping Max
+        g.drawString(Integer.toString(this.maxValue), (25 / 2) - 3, tepiAtas);
+        // Nilai Samping Mid
+        int nilaiMid = (tepiBawah - tepiAtas) / 2 + (this.maxValue - this.minValue);
+        g.drawString(Integer.toString(getMid()), (25 / 2) - 3, nilaiMid);
+        /* End Nilai Samping */
+
+        // Start Warna Garis
+        g.setColor(Color.blue);
+        // End Warna Garis
+        // Start Garis Y
+        g.drawLine(25, tepiBawah - 7, 25, tepiAtas);
+        // End Garis
+        // Start Garis X
+        g.drawLine(25, tepiBawah - 7, 247, tepiBawah - 7);
+        // End Garis X
+
+        /*
+         * Full = 192
+         * Spasi = 1
+         * Koordinat Y (Atas) = 45
+         * Koordinat Y (Bawah) = 237
+         */
+
+        // Start Bar Grafik
+        g.setColor(Color.red);
+
+        int ukuran = (tepiBawah - tepiAtas) / (this.maxValue - this.minValue);
+        int lebarBatang = 37;
+        String[] namaBulan = { "Jan", "Feb", "Mar", "Apr", "Mei", "Jun" };
+
+        for (int index = 0; index < this.arrValue.length; index++) {
+            int koorX = index * lebarBatang + 27;
+            int koorY = tepiAtas;
+            int tinggi = (int) (this.arrValue[index] * ukuran);
+            if (this.arrValue[index] >= 0) {
+                koorY += (int) ((this.maxValue - this.arrValue[index]) * ukuran);
+            } else {
+                koorY += (int) (this.maxValue * ukuran);
+                tinggi = -tinggi;
+            }
+            g.fillRect(koorX, koorY, lebarBatang - 1, tinggi);
+            g.drawString(namaBulan[index], koorX + 7, 250);
+        }
+        // End Bar Grafik
+    }
+
+
+    public static void main(String[] args) {
+        new Grafik();
     }
 }
